d2d9a0fb5d10faa37b034fd1ec6ce5ea94eeb140
vedantsinggh <vedantsinggh@gmail.com>
2025-01-14 03:20:36 +0530

    fixed install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ wget https://github.com/raysan5/raylib/releases/download/5.5/raylib-5.5_linux_am
 tar -xzvf ./raylib.tar.gz
 mv raylib-5.5_linux_amd64 raylib
 rm raylib.tar.gz
+mkdir -p include lib
 cp ./raylib/include/raylib.h ./include
 cp ./raylib/lib/libraylib.a ./lib
 rm -r raylib
