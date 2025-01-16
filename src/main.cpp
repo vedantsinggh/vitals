33dbe6bd893f0ac64caca9fe46a8d84fafd11af3
vedantsinggh <vedantsinggh@gmail.com>
2025-01-16 19:06:01 +0530

    Updated graph UI

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,20 @@
 #include "raylib.h"
-#include <cstdlib>
 #include <stdlib.h>
 
 #define MAX_DATA_POINTS 2000
 #define DISPLAY_POINT 40
 
-void DrawGraph(float *data, int dataCount, Rectangle graphArea) {
-	DrawRectangleLinesEx(graphArea, 2, GRAY);
-	int start = 0;
-    if (dataCount < 2) return; 
-	if (dataCount > DISPLAY_POINT) start = dataCount - DISPLAY_POINT;
+void DrawGraph(float *data, int dataCount, Vector3 color, Rectangle graphArea) {
+    DrawRectangleRounded(graphArea, 0.1, 10, Fade(SKYBLUE, 0.2f)); 
+    DrawRectangleLinesEx(graphArea, 2, Fade(DARKGRAY, 0.6f)); 
 
-	float minValue = 0;
-	float maxValue = 120;
+    if (dataCount < 2) return;
+
+    int start = 0;
+    if (dataCount > DISPLAY_POINT) start = dataCount - DISPLAY_POINT;
+
+    float minValue = 0;
+    float maxValue = 120;
     float range = maxValue - minValue;
     if (range == 0) range = 1;
 
@@ -22,44 +24,61 @@ void DrawGraph(float *data, int dataCount, Rectangle graphArea) {
         float x1 = graphArea.x + (i - start) * stepX;
         float y1 = graphArea.y + graphArea.height - ((data[i] - minValue) / range) * graphArea.height;
 
-
-
         float x2 = graphArea.x + (i - start + 1) * stepX;
         float y2 = graphArea.y + graphArea.height - ((data[i + 1] - minValue) / range) * graphArea.height;
 
-		Vector2 startPoint = {x1, y1};
-		Vector2 endPoint = {x2, y2};
+        Vector2 startPoint = {x1, y1};
+        Vector2 endPoint = {x2, y2};
 
-		DrawLineBezier(startPoint, endPoint, 4.0f,Color{ 33, 104, 166, (x1 - graphArea.x)/(float)(graphArea.width) * 255 + 25});
-        //DrawLineEx((Vector2){x1, y1}, (Vector2){x2, y2}, 2, BLUE);
+        DrawCircleGradient(startPoint.x, startPoint.y, 5, Color{color.x, color.y, color.z, 200}, RAYWHITE);
+        DrawLineBezier(startPoint, endPoint, 3.0f, Color{color.x, color.y, color.z, 200});
     }
 }
 
+struct Graph {
+    Vector3 color;
+    float *data;
+    int dataCount;
+};
+
 int main() {
-    float screenWidth = 800;
-    float screenHeight = 600;
+    int screenWidth = 800;
+    int screenHeight = 600;
 
-	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-    InitWindow(screenWidth, screenHeight, "Vitals");
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow(screenWidth, screenHeight, "Sexy Vitals UI");
 
-    float data[MAX_DATA_POINTS];
+    float data[MAX_DATA_POINTS] = {50, 60, 55, 70}; 
     int dataCount = 4;
 
-	SetTargetFPS(144);
+    Graph graphs[] = {
+        {{33, 104, 166}, data, dataCount}, 
+        {{32, 63, 51}, data, dataCount}    
+    };
+
+    int selected = 0;
+
+    SetTargetFPS(5);
 
     while (!WindowShouldClose()) {
+
 		screenHeight = GetScreenHeight();
 		screenWidth = GetScreenWidth();
         BeginDrawing();
-        ClearBackground((Color){ 200, 200, 200, 255 });
+        ClearBackground(Fade(RAYWHITE, 0.95f));
 
-        DrawText("Graph", 0.75 * screenWidth - (float)MeasureText("Graph", 20) / 2, 10, 20, GRAY);
-        Rectangle graphArea = {screenWidth/2, 0, screenWidth, screenHeight/2};
-        DrawGraph(data, dataCount, graphArea);
-		if(dataCount < MAX_DATA_POINTS){
-			data[dataCount] = rand() % 40 + 40;
-			dataCount++;
-		}
+		const float margin = 20;
+        DrawText("Vitals Dashboard", screenWidth / 2 - MeasureText("Vitals Dashboard", 30) / 2, screenHeight - 30 - margin, 30, DARKGRAY);
+
+        Rectangle graphArea = {screenWidth/2 - margin, margin, screenWidth/2 - margin, screenHeight/2 - margin};
+		DrawGraph(graphs[selected].data, graphs[selected].dataCount, graphs[selected].color, graphArea);
+
+        if (graphs[selected].dataCount < MAX_DATA_POINTS) {
+            graphs[selected].data[graphs[selected].dataCount] = rand() % 40 + 40;
+            graphs[selected].dataCount++;
+        }
+
+        DrawText("Press ESC to Exit", 20, screenHeight - 40, 20, GRAY);
 
         EndDrawing();
     }
